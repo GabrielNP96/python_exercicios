5528c2740c4d93f289c873eab62d294fd9603d15
GabrielNP96 <gabrielnp1996@gmail.com>
2024-03-14 21:00:47 -0300

    add 01_hello_world

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,2 +1,2 @@
 name = 'Gabriel'
-print(f'Hello World!\nMe chamo {name}, e estou aprendo a programar com python. ')
+print(f'Hello World!\nMe chamo {name}, e estou aprendo a programar com python.\nPython é divertido!\n1 + 2 = {1+2} ')
