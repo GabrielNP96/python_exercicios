67dbcbce44af26fe114d0beacd5c9d32509efcaf
GabrielNP96 <gabrielnp1996@gmail.com>
2024-03-14 21:14:42 -0300

    add atividade sobre soma

---
+++ 02_sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+def sum_of_two_numbers(number_one, number_two):
+    print(f'{number_one} + {number_two} = {number_one + number_two} ')
+
+try:
+    first_user_number = float(input('Digite um número: '))
+    second_user_number = float(input('Digite outro número: '))
+
+    sum_of_two_numbers(first_user_number, second_user_number)
+
+except:
+    print('Erro...\nvalor inválido...')
